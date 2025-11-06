e54fb59d394bde51a5212cc927b922f6b1868eb0
Neon <danielemmanuel257@gmail.com>
2025-11-06 08:24:27 +0100

    fixed httpservice

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,37 +1,53 @@
-import axios, { AxiosError } from "axios"
-import Cookies from "js-cookie"
+import axios, { AxiosError } from "axios";
+import Cookies from "js-cookie";
 
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 const httpService = axios.create({
   baseURL: BASE_URL,
   // withCredentials: true,
-})
+});
 
 export const unsecureHttpService = axios.create({
-    baseURL: `${BASE_URL}`,
-})
-
-unsecureHttpService.interceptors.response.use((data) => {
-    return data;
-}, async (error: AxiosError<any, unknown>) => {
-    return Promise.reject(error);
+  baseURL: `${BASE_URL}`,
 });
+
+unsecureHttpService.interceptors.response.use(
+  (data) => {
+    return data;
+  },
+  async (error: AxiosError<any, unknown>) => {
+    return Promise.reject(error);
+  }
+);
 
 httpService.interceptors.request.use(
   function (config: any) {
-    const token = Cookies.get("access_token") 
-    
+    const token = Cookies.get("access_token");
+
     if (token) {
-      config.headers["Authorization"] = "Bearer " +token
+      config.headers["Authorization"] = "Bearer " + token;
     }
-    return config
+    return config;
   },
   function (error: any) {
     // if (error.response.status === 500) {
     //   error.response.data.message = "Something wrong has happened. Try again later."
     // }
-    return Promise.reject(error)
-  },
-)
+    return Promise.reject(error);
+  }
+);
 
-export default httpService
+httpService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      Cookies.remove("access_token");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default httpService;
